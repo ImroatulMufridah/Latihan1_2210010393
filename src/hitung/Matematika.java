f5e21ec03f9f285ca90e93c78e3b7602968fd852
ImroatulMufridah <mufridahfidah@gmail.com>
2024-06-03 10:34:37 +0800

    membuat konstruktor dan set penjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,5 +1,16 @@
 package hitung;
 
 public class Matematika {
-     double (bil1,bil2);
+    //membuat variabel
+     private double bil1,bil2;
+     
+     //membuat konstruktor
+     public Matematika (double bil1, double bil2){
+         this.bil1 = bil1;
+         this.bil2 = bil2;
+     }
+     
+     public double setPenjumlahan(){
+         return bil1 + bil2;
+     }
 }
